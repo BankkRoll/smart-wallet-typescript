5b2eddebb294bf6efb65652c1e7856b325607d48
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-05-07 20:25:36 -0400

    Delete utils directory

--- utils/render/textUtils.ts
+++
@@ -1,9 +0,0 @@
-// utils/textUtils.ts
-
-export function convertURLsToMarkdownLinks(text: string) {
-    const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
-  
-    return text.replace(urlPattern, (url) => {
-      return `[${url}](${url})`;
-    });
-  }
